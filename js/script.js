6a44c0f933474a70167392f8f68047ee47330591
jordansatriopratama <jordansama0@gmail.com>
2023-12-11 14:34:55 +0700

    aos

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-const nav = document.getElementById("nav");
+const nav = document.getElementById('nav');
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
   scrollposition = window.scrollY;
   if (scrollposition >= 60) {
-    nav.classList.add("nav-dark");
+    nav.classList.add('nav-dark');
   } else if (scrollposition <= 60) {
-    nav.classList.remove("nav-dark");
+    nav.classList.remove('nav-dark');
   }
 });
+
+const imageskill = document.querySelectorAll('.imgskill');
+imageskill.forEach((img, i) => {
+  img.dataset.aos = 'flip-left';
+  img.dataset.aosDelay = i * 250;
+  img.dataset.aosDuration = 1000;
+  img.dataset.aosOffset = 400;
+});
+
+// src = 'https://unpkg.com/aos@next/dist/aos.js';
+// AOS.init();
